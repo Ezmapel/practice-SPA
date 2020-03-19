94b65eb908b3af905e45c66f65888737aa7744ea
ezmapel <ezmapel@EthanMapelsMBP.attlocal.net>
2020-03-19 15:59:50 -0500

    Add improved export default formatting in Blog.js view

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -1,34 +1,30 @@
 import { capitalize } from "lodash";
 
-export default st => `<section id="blog">
-${st.posts
-  .map(post => {
-    return `
-      <div class="blog-post">
-        <h4>${capitalize(post.title)} by User ${post.userId}</h4>
-        <p>${capitalize(post.body)}</p>
-      </div>`;
-  })
-  .join("")}
-</section>`;
-
-// WHY DOESN'T THE BELOW WORK? DO I HAVE TO EXPORT THE FUNCTION?
-
 // export default st => `<section id="blog">
 // ${st.posts
 //   .map(post => {
-//     formatBlogPost(post);
+//     return `
+//       <div class="blog-post">
+//         <h4>${capitalize(post.title)} by User ${post.userId}</h4>
+//         <p>${capitalize(post.body)}</p>
+//       </div>`;
 //   })
 //   .join("")}
 // </section>`;
 
-// function formatBlogPost(post) {
-//   return `
-//   <div class="blog-post">
-//     <h4>${capitalize(post.title)} by User ${post.userId}</h4>
-//     <p>${capitalize(post.body)}</p>
-//   </div>`;
-// }
+// Had to remove the curly braces around formatBlogPost(post) to make it work...or explicitly return it
+
+export default st => `<section id="blog">
+${st.posts.map(post => formatBlogPost(post)).join("")}
+</section>`;
+
+function formatBlogPost(post) {
+  return `
+  <div class="blog-post">
+    <h4>${capitalize(post.title)} by User ${post.userId}</h4>
+    <p>${capitalize(post.body)}</p>
+  </div>`;
+}
 
 // SAME THING AS ABOVE, JUST ARROW NOTATION
 // const formatBlogPost = post => `
